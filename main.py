2b1f6daab9fb482dfd8677149d6406cb5b3e703a
jonathan <jonathancolacocarr@gmail.com>
2022-08-17 09:30:53 -0400

    Train on 500k trajectories

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     model = get_reward_model(num_windows, memory_window_length)
 
     # Load the trajectories and their corresponding returns
-    trajectories, returns = load_trajectories_and_returns_from_env('ToyCenteredArray', 200_000, T)
-    # trajectories, returns = load_trajectories_and_returns_from_file('data_acquisition/data/toy_centered_array', 1_000_000, T)
+    #trajectories, returns = load_trajectories_and_returns_from_env('ToyCenteredArray', 200_000, T)
+    trajectories, returns = load_trajectories_and_returns_from_file('data_acquisition/data/toy_centered_array', 500_000, T)
     x_train, y_train = process_trajectories(trajectories, returns, memory_window_length, shuffle=True)
 
     train_model(model, x_train, y_train)
